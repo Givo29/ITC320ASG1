41e1952b97bb004826a87c935b5f41a17f1591c5
Joel Giovinazzo <joelgiovinazzo@gmail.com>
2021-08-20 16:52:33 +1000

    Make car drift around building, clean up directory

--- index.js
+++ index.js
@@ -28,9 +28,105 @@ window.onload = async (_) => {
   gl = WebGLUtils.setupWebGL(canvas);
   if (!gl) alert("WebGL isn't available");
 
-  const house = new objectParser("Assets/meshes/H1.obj");
-
-  const houseTexture = await parseTGA(gl, "Assets/textures/H1.tga");
+  const shapes = [
+    // Floor
+    await parseObject(
+      gl,
+      "floor",
+      "Assets/meshes/floor.obj",
+      "Assets/textures/floor.tga",
+      [50, 50, 50]
+    ),
+    // Houses
+    await parseObject(
+      gl,
+      "h1",
+      "Assets/meshes/H1.obj",
+      "Assets/textures/H1.tga",
+      [1, 1, 1],
+      [1, 2, 0],
+      90
+    ),
+    await parseObject(
+      gl,
+      "h1",
+      "Assets/meshes/H1.obj",
+      "Assets/textures/H1.tga",
+      [2, 2, 2],
+      [-5, 2, 2]
+    ),
+    await parseObject(
+      gl,
+      "h1",
+      "Assets/meshes/H1.obj",
+      "Assets/textures/H1.tga",
+      [1.2, 1.2, 1.2],
+      [-2, 2, -.5], //! change 20 to 2
+      -45
+    ),
+    await parseObject(
+      gl,
+      "h2",
+      "Assets/meshes/H2.obj",
+      "Assets/textures/H2.tga",
+      [2, 2, 2],
+      [0.5, 2, 1],
+      -25
+    ),
+    await parseObject(
+      gl,
+      "h2",
+      "Assets/meshes/H2.obj",
+      "Assets/textures/H2.tga",
+      [1.5, 1.5, 1.5],
+      [-2, 2, -5],
+      45
+    ),
+    await parseObject(
+      gl,
+      "h2",
+      "Assets/meshes/H2.obj",
+      "Assets/textures/H2.tga",
+      [1, 4, 1],
+      [5, 2, 5],
+      10
+    ),
+    await parseObject(
+      gl,
+      "h3",
+      "Assets/meshes/H3.obj",
+      "Assets/textures/H3_1.tga",
+      [3, 3, 3],
+      [5.5, 0, 2.5]
+    ),
+    await parseObject(
+      gl,
+      "h3",
+      "Assets/meshes/H3.obj",
+      "Assets/textures/H3_1.tga",
+      [5, 5, 5],
+      [-3, 0, 22],
+      45
+    ),
+    await parseObject(
+      gl,
+      "h3",
+      "Assets/meshes/H3.obj",
+      "Assets/textures/H3_1.tga",
+      [1.5, 1.5, 1.5],
+      [-5, 0, 4],
+      15
+    ),
+    // Car
+    await parseObject(
+      gl,
+      "car",
+      "Assets/meshes/car.obj",
+      "Assets/textures/car_1.tga",
+      [1, 1, 1],
+      [-3, 0, -5.5]
+    ),
+  ];
 
   gl.viewport(0, 0, canvas.width, canvas.height);
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -41,54 +137,75 @@ window.onload = async (_) => {
   gl.uniform1i(gl.getUniformLocation(program, "myTexture"), 0);
   gl.activeTexture(gl.TEXTURE0);
 
-  // Position buffer
-  const posBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, posBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, flatten(house.pPoints), gl.STATIC_DRAW);
-
-  const vPos = gl.getAttribLocation(program, "aPosition");
-  gl.vertexAttribPointer(vPos, 3, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(vPos);
-
-  // Colour buffer
-  const colBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, colBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, flatten(house.tPoints), gl.STATIC_DRAW);
-
-  const vCol = gl.getAttribLocation(program, "aTextureCoordinate");
-  gl.vertexAttribPointer(vCol, 2, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(vCol);
-
   const worldMatrixLocation = gl.getUniformLocation(program, "umWorldMatrix");
   gl.enable(gl.DEPTH_TEST);
 
-  setInterval(render, 16, house, houseTexture, worldMatrixLocation);
+  // setInterval(render, 16, house, houseTexture, worldMatrixLocation);
+  setInterval(render, 16, shapes, worldMatrixLocation, program);
 };
 
-function render(house, houseTexture, worldMatrixLocation) {
+function render(shapes, worldMatrixLocation, program) {
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-  if (houseTexture === null) return;
-  gl.bindTexture(gl.TEXTURE_2D, houseTexture);
-
   let projectionMatrix = perspective(45, 640.0 / 640.0, 2.1, 10000.0);
-  let viewMatrix = lookAt([0, 0, -20], [0, 0, 10], [0, 1, 0]);
+  let viewMatrix = lookAt([1, 4, -15], [0, 0, 10], [0, 1, 0]);
   let projViewMatrix = mult(projectionMatrix, viewMatrix);
-  let initMatrix = mult(
-    scalem(1.0 / house.radius, 1.0 / house.radius, 1.0 / house.radius),
-    translate(-house.center[0], -house.center[1], -house.center[2])
-  );
 
-  initMatrix = scalem(
-    2.0 / house.radius,
-    2.0 / house.radius,
-    2.0 / house.radius
-  );
+  shapes.forEach((shape) => {
+    if (shape.type === "car") {
+      shape.rotation--;
+      shape.pos = [shape.pos[0], shape.pos[1], shape.pos[2]];
+    }
 
-  const worldMatrix = mult(
-    projViewMatrix,
-    mult(rotate(45, [0, 1, 0]), initMatrix)
-  );
-  gl.uniformMatrix4fv(worldMatrixLocation, gl.FALSE, flatten(worldMatrix));
-  gl.drawArrays(gl.TRIANGLES, 0, house.pPoints.length);
+    let initMatrix = mult(
+      scalem(1.0 / shape.radius, 1.0 / shape.radius, 1.0 / shape.radius),
+      translate(-shape.center[0] + 10, -shape.center[1] + 100, -shape.center[2])
+    );
+
+    // Position buffer
+    const posBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, posBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(shape.pPoints), gl.STATIC_DRAW);
+
+    const vPos = gl.getAttribLocation(program, "aPosition");
+    gl.vertexAttribPointer(vPos, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPos);
+
+    // Colour buffer
+    const colBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(shape.tPoints), gl.STATIC_DRAW);
+
+    const vCol = gl.getAttribLocation(program, "aTextureCoordinate");
+    gl.vertexAttribPointer(vCol, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vCol);
+
+    if (shape.texture === null) return;
+    gl.bindTexture(gl.TEXTURE_2D, shape.texture);
+
+    initMatrix = scalem(
+      shape.scale[0] / shape.radius,
+      shape.scale[0] / shape.radius,
+      shape.scale[0] / shape.radius
+    );
+    let worldMatrix;
+    if (shape.type !== "car") {
+      worldMatrix = mult(
+        projViewMatrix,
+        mult(
+          translate(shape.pos[0], shape.pos[1], shape.pos[2]),
+          mult(rotate(shape.rotation, [0, 1, 0]), initMatrix)
+        )
+      );
+    } else {
+      worldMatrix = mult(
+        projViewMatrix,
+        mult(
+          rotate(shape.rotation, [0, 1, 0]),
+          mult(translate(shape.pos[0] + 2.5, shape.pos[1], shape.pos[2] + 2.5), initMatrix)
+        )
+      );
+    }
+    gl.uniformMatrix4fv(worldMatrixLocation, gl.FALSE, flatten(worldMatrix));
+    gl.drawArrays(gl.TRIANGLES, 0, shape.pPoints.length);
+  });
 }
--- objectParser.js
+++ objectParser.js
@@ -1,22 +1,59 @@
-function objectParser(file, texture, scale=[1, 1, 1], pos=[0, 0, 0], rot = 45) {
-  this.pPoints = [];
-  this.tPoints = [];
-  this.span = [];
-  this.radius = 0;
-  this.center = [];
-  this.scale = scale;
-  this.pos = pos;
-  this.rotation = rot;
-  this.texture = texture;
+async function parseObject(
+  gl,
+  type,
+  objFile,
+  textureFile,
+  scale = [1, 1, 1],
+  pos = [0, 0, 0],
+  rot = 45
+) {
+  /* Setting up Object vertices */
+  const obj = await fetch(objFile).then((response) =>
+    response.status === 200 ? response.text() : null
+  );
 
-  const lines = loadFileAJAX(file).split("\n");
-  if (!lines) alert(`Could not get data from ${file}`);
+  // Making sure the object is loaded
+  if (obj === null) {
+    alert(`Error loading object file ${objFile}`);
+    return;
+  }
+  // Formatting data correctly
+  const objLines = obj.split("\n");
+  const object = setupObject(objLines);
+
+  /* Setting up Texture */
+  const encodedTexture = await fetch(textureFile).then((response) =>
+    response.status === 200 ? response.arrayBuffer() : null
+  );
+
+  // Making sure the texture is loaded
+  if (encodedTexture === null) {
+    alert(`Error loading texture file ${textureFile}`);
+    return;
+  }
+  // Formatting data correctly
+  const textureData = new Uint8Array(encodedTexture);
+  const texture = setupTexture(gl, textureData);
+
+  object.scale = scale;
+  object.pos = pos;
+  object.rotation = rot;
+  object.texture = texture;
+  object.type = type;
+
+  return object;
+}
+
+function setupObject(data) {
+  let pPoints = [];
+  let tPoints = [];
 
   let minX, maxX, minY, maxY, minZ, maxZ;
-  let vertices = [];
+  let pVertices = [];
   let tVertices = [];
 
-  lines.forEach((line) => {
+  data.forEach((line) => {
+    // Object Coordinates
     if (line[0] === "v" && line[1] === " ") {
       const vertex = line.split(" ");
       const newPosition = [
@@ -25,9 +62,9 @@ function objectParser(file, texture, scale=[1, 1, 1], pos=[0, 0, 0], rot = 45) {
         parseFloat(vertex[3]),
       ];
 
-      vertices.push(newPosition);
+      pVertices.push(newPosition);
 
-      if (vertices.length === 1) {
+      if (pVertices.length === 1) {
         minX = maxX = newPosition[0];
         minY = maxY = newPosition[1];
         minZ = maxZ = newPosition[2];
@@ -41,6 +78,7 @@ function objectParser(file, texture, scale=[1, 1, 1], pos=[0, 0, 0], rot = 45) {
       }
     }
 
+    // Texture Coordinates
     if (line[0] === "v" && line[1] === "t") {
       const vertex = line.split(" ");
       const newPosition = [parseFloat(vertex[1]), parseFloat(vertex[2])];
@@ -52,16 +90,64 @@ function objectParser(file, texture, scale=[1, 1, 1], pos=[0, 0, 0], rot = 45) {
 
       face.slice(1).forEach((element) => {
         let vertex = element.split("/");
-        this.pPoints.push(vertices[vertex[0] - 1]);
-        this.tPoints.push(tVertices[vertex[1] - 1]);
+        pPoints.push(pVertices[vertex[0] - 1]);
+        tPoints.push(tVertices[vertex[1] - 1]);
       });
     }
   });
-  this.span = [maxX - minX, maxY - minY, maxZ - minZ];
-  this.center = [((maxX + minX) / 2.0), (maxY + minY) / 2.0, (maxZ + minZ) / 2.0];
-  this.radius = Math.sqrt(
-    (this.center[0] - minX) * (this.center[0] - minX) +
-      (this.center[1] - minY) * (this.center[1] - minY) +
-      (this.center[2] - minZ) * (this.center[2] - minZ)
+
+  const  center = [(maxX + minX) / 2, (maxY + minY) / 2, (maxZ + minZ) / 2];
+  return {
+    pPoints: pPoints,
+    tPoints: tPoints,
+    span: [maxX - minX, maxY - minY, maxZ - minZ],
+    center: center,
+    radius: Math.sqrt(
+      (center[0] - minX) * (center[0] - minX) +
+      (center[1] - minY) * (center[1] - minY) +
+      (center[2] - minZ) * (center[2] - minZ)
+  )
+  }
+}
+
+function setupTexture(gl, data) {
+  let texture;
+
+  const width = data[12] + (data[13] << 8);
+  const height = data[14] + (data[15] << 8);
+  const pixelDepth = data[16];
+  const channelNum = pixelDepth / 8;
+
+  for (let i = 0; i < width * height * channelNum; i += channelNum) {
+    data[i] = data[i + 18 + 2];
+    data[i + 1] = data[i + 18 + 1];
+    data[i + 2] = data[i + 18];
+  }
+
+  texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGB,
+    width,
+    height,
+    0,
+    gl.RGB,
+    gl.UNSIGNED_BYTE,
+    data
   );
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+  gl.texParameteri(
+    gl.TEXTURE_2D,
+    gl.TEXTURE_MIN_FILTER,
+    gl.LINEAR_MIPMAP_NEAREST
+  );
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+
+  gl.generateMipmap(gl.TEXTURE_2D);
+
+  return texture;
 }
